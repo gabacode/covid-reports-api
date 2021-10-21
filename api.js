887148a5f96ce2d8b77a0338da53a72c019c6a36
Gabriele Scalici <gaba41@gmail.com>
2021-10-22 00:16:20 +0200

    initial

---
+++ api.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const csv = require('csvtojson')
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+const incidenza = './dati/incidenza_comuni_distretti.csv';
+const vaccinati = './dati/vaccinati.csv';
+
+const parse = (f) => {
+    let file = csv().fromFile(f)
+    return file
+}
+
+const filter = (array, query) => {
+    return array.filter(x =>  JSON.stringify(x).toLowerCase().indexOf(query.toLowerCase()) !== -1)
+}
+
+const comune = (array, query) => {
+    return array.filter(x => x.Comune.toLowerCase().includes(query.toLowerCase()))
+}
+
+app.use(cors({ origin: '*' }));
+
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get('/incidenza', async (req, res) => {
+    let query = req.query
+    const data = await parse(incidenza)
+    if(typeof query.comune !== 'undefined' || typeof query.distretto !== 'undefined'){
+        try {
+            res.send(comune(data, query.comune))
+        }
+        catch(e){
+            console.log(e)
+        }
+    } else {
+        try {
+            res.send(data)
+        }
+        catch(e){
+            res.sendStatus(e)
+        }
+    }
+})
+
+app.get('/vaccini', async (req, res) => {
+    let query = req.query.q
+    const data = await parse(vaccinati)
+    if(query){
+        try {
+            res.send(filter(data, query))
+        }
+        catch(e){
+            res.sendStatus(e)
+        }
+    } else {
+        try {
+            res.send(data)
+        }
+        catch(e){
+            res.sendStatus(e)
+        }
+    }
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
